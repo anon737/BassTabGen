18f31c2ee989f21805da113401478a9511aeaa37
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2025-04-18 17:32:54 +0200

    test AlphaTab

--- script.js
+++ script.js
@@ -76,3 +76,69 @@ alphaTabInstance.playerPositionChanged.on((args) => {
   const seconds = Math.floor(args.position % 60);
   currentTimeDisplay.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 });
+
+const playerIndicator = wrapper.querySelector(
+  ".at-controls .at-player-progress"
+);
+api.soundFontLoad.on((e) => {
+  const percentage = Math.floor((e.loaded / e.total) * 100);
+  playerIndicator.innerText = percentage + "%";
+});
+api.playerReady.on(()=>{
+  playerIndicator.style.display = 'none';
+});
+
+const playPause = wrapper.querySelector(
+  ".at-controls .at-player-play-pause"
+);
+const stop = wrapper.querySelector(".at-controls .at-player-stop");
+playPause.onclick = (e) => {
+  if (e.target.classList.contains("disabled")) {
+    return;
+  }
+  api.playPause();
+};
+stop.onclick = (e) => {
+  if (e.target.classList.contains("disabled")) {
+    return;
+  }
+  api.stop();
+};
+api.playerReady.on(() => {
+  playPause.classList.remove("disabled");
+  stop.classList.remove("disabled");
+});
+api.playerStateChanged.on((e) => {
+  const icon = playPause.querySelector("i.fas");
+  if (e.state === alphaTab.synth.PlayerState.Playing) {
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+  } else {
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+  }
+});
+
+function formatDuration(milliseconds) {
+  let seconds = milliseconds / 1000;
+  const minutes = (seconds / 60) | 0;
+  seconds = (seconds - minutes * 60) | 0;
+  return (
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(seconds).padStart(2, "0")
+  );
+}
+
+const songPosition = wrapper.querySelector(".at-song-position");
+let previousTime = -1;
+api.playerPositionChanged.on((e) => {
+  // reduce number of UI updates to second changes.
+  const currentSeconds = (e.currentTime / 1000) | 0;
+  if (currentSeconds == previousTime) {
+    return;
+  }
+
+  songPosition.innerText =
+    formatDuration(e.currentTime) + " / " + formatDuration(e.endTime);
+});
